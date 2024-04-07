2289b247ced80ba10e69a4b421df0a6dc6410c8c
Casper <Casperbdj@gmail.com>
2024-04-07 22:44:37 +0200

    Opgaver kan kun laves af en robot ad gangen

--- main_robot.cpp
+++ main_robot.cpp
@@ -8,16 +8,18 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName("localhost");
     db.setDatabaseName("robot_worker");
-    db.setUserName("sammy");  
+    db.setUserName("sammy"); 
     db.setPassword("#Superdeadcasp2004");  
     if (!db.open()) {
         qDebug() << "Database connection failed";
         return -1;
     }
-
+    
+  
     QSqlQuery query;
     QString robotName = "MyRobot";
     if (!query.exec(QString("INSERT INTO robots (name, current_task) VALUES ('%1', 'Starting')").arg(robotName))) {
@@ -27,7 +29,7 @@ int main(int argc, char *argv[])
     qDebug() << "Robot named" << robotName << "has been created in the database.";
 
    
-    if (query.exec("SELECT * FROM task")) {
+    if (query.exec("SELECT * FROM task WHERE is_assigned = FALSE")) {
         while (query.next()) {
             int id = query.value(0).toInt();
             QString taskName = query.value(1).toString();
@@ -40,32 +42,28 @@ int main(int argc, char *argv[])
 
     
     int taskId;
-    std::cout << "Enter the ID of the task you wish to remove: ";
+    std::cout << "Enter the ID of the task you wish to assign: ";
     std::cin >> taskId;
 
-    query.prepare("DELETE FROM task WHERE task_id = :task_id");
-    query.bindValue(":task_id", taskId);
-    if (query.exec()) {
-        if(query.numRowsAffected() > 0) {
-            qDebug() << "The task with ID" << taskId << "has now been completed and removed from the database.";
-        } else {
-            qDebug() << "No tasks found with the given ID.";
-        }
-    } else {
-        qDebug() << "Error removing task:" << query.lastError().text();
-    }
-
     
-    std::cout << "Remaining tasks" << std::endl;
-    if (query.exec("SELECT * FROM task")) {
-        while (query.next()) {
-            int id = query.value(0).toInt();
-            QString taskName = query.value(1).toString();
-            QString time = query.value(2).toString();
-            qDebug() << "Task ID:" << id << "Name:" << taskName << "Time:" << time;
+    query.prepare("SELECT is_assigned FROM task WHERE task_id = :task_id");
+    query.bindValue(":task_id", taskId);
+    if (query.exec() && query.next()) {
+        bool isAssigned = query.value(0).toBool();
+        if (!isAssigned) {
+            
+            query.prepare("UPDATE task SET is_assigned = TRUE WHERE task_id = :task_id");
+            query.bindValue(":task_id", taskId);
+            if (query.exec()) {
+                qDebug() << "The task with ID" << taskId << "is now assigned to the robot.";
+            } else {
+                qDebug() << "Error updating task assignment status:" << query.lastError().text();
+            }
+        } else {
+            qDebug() << "This task is already assigned to another robot.";
         }
     } else {
-        qDebug() << "Error executing query:" << query.lastError().text();
+        qDebug() << "Error checking task assignment status:" << query.lastError().text();
     }
 
     
@@ -77,3 +75,4 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
